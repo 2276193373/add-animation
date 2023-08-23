89d1c256c63b5ce518144247077195bcd1b4202e
2276193373 <2276193373@qq.com>
2023-08-23 18:17:48 +0800

    新增options参数,unobserve参数移到最后

--- index.js
+++ index.js
@@ -4,22 +4,36 @@
  * @param {string|string[]} startAnimationClass 起始状态类名
  * @param {string|string[]} endAnimationClass 结束状态类名
  * @param {boolean} unobserve observe后是否取消observe, 默认为 true
+ * @param {object} options 同 IntersectionObserver 构造器的 options 参数
  * @returns {object} 返回一个 IntersectionObserver 实例
  */
-export function addAnimation(selectors, startAnimationClass, endAnimationClass, unobserve = true) {
+
+export function addAnimation(selectors, startAnimationClass, endAnimationClass, options, unobserve = true) {
+  const isOptions = 
+    Object.prototype.toString.call(options) === '[object Object]' ||
+    Object.prototype.toString.call(options) === '[object Undefined]' ||
+    Object.prototype.toString.call(options) === '[object Null]'
+    
+  if (!isOptions) {
+    console.error('addAnimation function: options 参数类型必须是对象!')
+    options = {}
+  }
   const io = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-      if (entry.intersectionRatio > 0) {
+      if (entry.isIntersecting) {
         entry.target.classList.add(...[endAnimationClass].flat())
         if (unobserve) {
           io.unobserve(entry.target)
         }
+      } else {
+        // 当元素移出视口时, 将元素的类重置为初始时的状态, 即 移除结束状态的类名
+        entry.target.classList.remove(...[endAnimationClass].flat())
       }
     })
-  })
+  }, options)
   const elements = document.querySelectorAll(selectors)
   if (!elements.length) {
-    console.error('addAnimation function: 所获取的 elements 为空, 请检查所传的选择器的名称是否正确或异步问题')
+    console.error('addAnimation function: 获取不到目标元素, 请检查所传的 [选择器名称] 是否正确或 [异步问题]')
   }
   elements.forEach(el => {
     el.classList.add(...[startAnimationClass].flat())
